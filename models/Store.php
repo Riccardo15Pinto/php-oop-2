0cb68e6b589e1d988baa8daba6d179dec40e0d1a
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-24 16:54:56 +0200

    fix store

--- models/Store.php
+++ models/Store.php
@@ -1,24 +1,12 @@
 <?php
 
-require_once __DIR__ . '/Article.php';
 
-class Store extends Article
+class Store
 {
-    private $product;
+    public $product;
 
     public function __construct($product)
     {
-        $this->setProduct($product);
-    }
-
-    public function getProduct()
-    {
-        return $this->product;
-    }
-
-    private function setProduct($target)
-    {
-        if (!$target instanceof Article && !strlen($target)) return false;
-        return $target;
+        $this->product = $product;
     }
 }
