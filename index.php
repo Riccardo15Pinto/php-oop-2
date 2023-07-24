5407ab6c3c9fcb4ed654033df1de6a907f161119
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-24 17:48:27 +0200

    print in page

--- index.php
+++ index.php
@@ -6,10 +6,12 @@ include_once __DIR__ . '/models/Customer.php';
 
 $dog = new Customer('dog');
 
-$dog_article = new Article('Royal Canin Mini Adult', 'https://arcaplanet.vtexassets.com/arquivos/ids/243820/royal-canin-size-cane-mini-adult.jpg', 545, 43.99, 'prociutto,riso', $dog);
-$cat_article = new Article('Royal Canin Mini Adult', 'https://arcaplanet.vtexassets.com/arquivos/ids/243820/royal-canin-size-cane-mini-adult.jpg', 545, 43.99, 'prociutto,riso', $dog);
-$bird_article = new Article('Royal Canin Mini Adult', 'https://arcaplanet.vtexassets.com/arquivos/ids/243820/royal-canin-size-cane-mini-adult.jpg', 545, 43.99, 'prociutto,riso', $dog);
+$dog_article = new Article('Royal Canin Mini Adult', 'https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', 545, 43.99, 'prociutto,riso', $dog);
+$cat_article = new Article('Royal Canin Mini Adult', 'https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', 545, 43.99, 'prociutto,riso', $dog);
+$bird_article = new Article('Royal Canin Mini Adult', 'https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', 545, 43.99, 'prociutto,riso', $dog);
 
+var_dump($dog);
+var_dump($dog_article);
 $shop_article = [$dog_article, $cat_article, $bird_article];
 
 $store = new Store($shop_article);
@@ -32,12 +34,14 @@ var_dump($store);
     <ul>
         <?php foreach ($store->product as $article) : ?>
             <li>
+                <img src='<?= $article->getImage() ?>' alt="">
                 <h1><?= $article->getName() ?></h1>
-                <img src="<?= $article->getImage() ?>" alt="">
-                <?php foreach ($article as $type->tipology) : ?>
+                <h1><?= $article->getWeight() ?></h1>
+                <h1><?= $article->getPrice() ?></h1>
+                <h1><?= $article->getIngredients() ?></h1>
+                <h1><?= $article->tipology->getTipology() ?></h1>
 
-                    <h1><?= $type->getTipology() ?></h1>
-                <? endforeach ?>
+
             </li>
         <? endforeach ?>
     </ul>
